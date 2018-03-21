ff045db0782fee4d79edbad3421355237a2e2995
Lukáš Kmoch <lukas.kmoch@designeo.cz>
2018-03-21 14:32:24 +0100

    Fix MovieService

--- BrnoMvc1.Business/Services/MovieService.cs
+++ BrnoMvc1.Business/Services/MovieService.cs
@@ -9,18 +9,15 @@ namespace BrnoMvc1.Business.Services
     public class MovieService : IMovieService
     {
         private readonly IEmailService emailService;
-        private readonly ApplicationDbContext applicationDbContext;
 
-        public MovieService(IEmailService emailService, ApplicationDbContext applicationDbContext)
+        public MovieService(IEmailService emailService)
         {
             this.emailService = emailService;
-            this.applicationDbContext = applicationDbContext;
         }
 
         public void CreateFilm()
         {
             // TODO create
-            applicationDbContext
             this.emailService.Send("Email has been created");
         }
     }
